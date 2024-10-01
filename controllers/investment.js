61238be2d6285ddebe23508e5acec2ac68560cd9
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-10-01 13:25:33 +0530

    Get request using ID

--- controllers/investment.js
+++ controllers/investment.js
@@ -6,28 +6,66 @@ const db = require("../dbConfig");
  * @param {*} res
  * @returns
  */
-const getAllInvestments = async(req, res) => {
-    try {
-        const data = await db.query(`SELECT * FROM invested_sip`);
-        if (!data) {
-            return res.status(404).send({
-                success: false,
-                message: "No investments found",
-            });
-        }
-        res.status(200).send({
-            success: true,
-            message: "Investments Data fetched successfully",
-            totalInvestments: data[0].length,
-            investments: data[0],
-        });
-    } catch (error) {
-        console.log(`error fetching data from database, ${error}`);
-        res.status(500).send({
-            success: false,
-            message: error,
-        });
+const getAllInvestments = async (req, res) => {
+  try {
+    const data = await db.query(`SELECT * FROM invested_sip`);
+    if (!data) {
+      return res.status(404).send({
+        success: false,
+        message: "No investments found",
+      });
     }
+    res.status(200).send({
+      success: true,
+      message: "Investments Data fetched successfully",
+      totalInvestments: data[0].length,
+      investments: data[0],
+    });
+  } catch (error) {
+    console.log(`error fetching data from database, ${error}`);
+    res.status(500).send({
+      success: false,
+      message: error,
+    });
+  }
+};
+
+/**
+ * GET request using ID
+ * @param {*} req
+ * @param {*} res
+ * @returns
+ */
+const getInvestmentById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log(`id == ${id}`);
+    if (!id) {
+      return res.status(400).send({
+        success: false,
+        message: "SIP ID is required !",
+      });
+    }
+    const data = await db.query(`SELECT * FROM invested_sip WHERE sip_id = ?`, [
+      id,
+    ]);
+    if (!data) {
+      return res.status(404).send({
+        success: false,
+        message: "Investment Data not found",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: data[0],
+    });
+  } catch (error) {
+    console.log(`Error fetching investment by ID`);
+    res.status(500).send({
+      success: false,
+      message: "Failed to find investment by ID",
+    });
+  }
 };
 
 /**
@@ -36,52 +74,54 @@ const getAllInvestments = async(req, res) => {
  * @param {*} res
  * @returns
  */
-const handleAddNewInvestment = async(req, res) => {
-    try {
-        const {
-            sip_id,
-            sip_name,
-            amount_invested,
-            frequency,
-            platform,
-            avg_interest,
-        } = req.body;
+const handleAddNewInvestment = async (req, res) => {
+  try {
+    const {
+      sip_id,
+      sip_name,
+      amount_invested,
+      frequency,
+      platform,
+      avg_interest,
+    } = req.body;
 
-        if (!sip_id ||
-            !sip_name ||
-            !amount_invested ||
-            !frequency ||
-            !platform ||
-            !avg_interest
-        ) {
-            console.log(`All data are required to add new Investment !`);
-            return res.status(400).send({
-                success: false,
-                message: "All Fields are required !",
-            });
-        }
-
-        const data = await db.query(
-            `INSERT INTO invested_sip (sip_id, sip_name, amount_invested, frequency, platform, avg_interest) VALUES (?, ?, ?, ?, ?, ?)`, [sip_id, sip_name, amount_invested, frequency, platform, avg_interest]
-        );
-        if (!data) {
-            console.log(`Error adding Investment`);
-            res.status(404).send({
-                success: false,
-                message: "Failed to Add Investment",
-            });
-        }
-        res.status(201).send({
-            success: true,
-            message: "Investment added successfully",
-        });
-    } catch (error) {
-        console.log(`Error adding new Investment, ${error}`);
-        res.status(500).send({
-            success: false,
-            message: error,
-        });
+    if (
+      !sip_id ||
+      !sip_name ||
+      !amount_invested ||
+      !frequency ||
+      !platform ||
+      !avg_interest
+    ) {
+      console.log(`All data are required to add new Investment !`);
+      return res.status(400).send({
+        success: false,
+        message: "All Fields are required !",
+      });
     }
+
+    const data = await db.query(
+      `INSERT INTO invested_sip (sip_id, sip_name, amount_invested, frequency, platform, avg_interest) VALUES (?, ?, ?, ?, ?, ?)`,
+      [sip_id, sip_name, amount_invested, frequency, platform, avg_interest]
+    );
+    if (!data) {
+      console.log(`Error adding Investment`);
+      res.status(404).send({
+        success: false,
+        message: "Failed to Add Investment",
+      });
+    }
+    res.status(201).send({
+      success: true,
+      message: "Investment added successfully",
+    });
+  } catch (error) {
+    console.log(`Error adding new Investment, ${error}`);
+    res.status(500).send({
+      success: false,
+      message: error,
+    });
+  }
 };
 
 /**
@@ -90,41 +130,42 @@ const handleAddNewInvestment = async(req, res) => {
  * @param {*} res
  * @returns
  */
-const handleDeleteInvestmentById = async(req, res) => {
-    try {
-        const { sip_id } = req.body;
-        if (!sip_id) {
-            console.log(`SIP ID is required to delete the investment`);
-            return res.status(400).send({
-                success: false,
-                message: "SIP ID is required to delete any investment",
-            });
-        }
-
-        const data = await db.query(`DELETE FROM invested_sip WHERE sip_id = ?`, [
-            sip_id,
-        ]);
-        if (!data) {
-            res.status(404).send({
-                success: false,
-                message: "Investment not found or already deleted",
-            });
-        }
-        res.status(200).send({
-            success: true,
-            message: "Investment deleted successfully",
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-            success: false,
-            message: "Error Deleting Investment",
-        });
+const handleDeleteInvestmentById = async (req, res) => {
+  try {
+    const { sip_id } = req.body;
+    if (!sip_id) {
+      console.log(`SIP ID is required to delete the investment`);
+      return res.status(400).send({
+        success: false,
+        message: "SIP ID is required to delete any investment",
+      });
     }
+
+    const data = await db.query(`DELETE FROM invested_sip WHERE sip_id = ?`, [
+      sip_id,
+    ]);
+    if (!data) {
+      res.status(404).send({
+        success: false,
+        message: "Investment not found or already deleted",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "Investment deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error Deleting Investment",
+    });
+  }
 };
 
 module.exports = {
-    getAllInvestments,
-    handleAddNewInvestment,
-    handleDeleteInvestmentById,
+  getAllInvestments,
+  handleAddNewInvestment,
+  handleDeleteInvestmentById,
+  getInvestmentById,
 };
--- routes/investment.js
+++ routes/investment.js
@@ -3,12 +3,15 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    getAllInvestments,
-    handleAddNewInvestment,
-    handleDeleteInvestmentById,
+  getAllInvestments,
+  handleAddNewInvestment,
+  handleDeleteInvestmentById,
+  getInvestmentById,
 } = require("../controllers/investment");
 
 router.get("/", getAllInvestments);
+
+router.get("/:id", getInvestmentById);
 
 router.post("/addInvestment", handleAddNewInvestment);
 
